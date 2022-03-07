5e0b7517fafc80e94bfc9ea1a5f29e78ee627458
Vito <farinavito@gmail.com>
2022-03-07 13:11:11 +0100

    added functionality to the button

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import 'bulma/css/bulma.css'
 import Link from 'next/link'
 
 const Homepage = () => {
+
     return(
+
         //outside container
         <div className={styles.main}>
             
@@ -96,7 +98,9 @@ const Homepage = () => {
                             </p>
                             <p className="box has-background-black-ter pt-4 pb-3">
                               <div className='columns is-centered'>
-                                <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                <Link href="/use-cases">
+                                  <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                </Link>
                               </div>
                             </p>
                           </p>
@@ -110,7 +114,9 @@ const Homepage = () => {
                             </p>
                             <p className="box has-background-black-ter pt-4 pb-3">
                               <div className='columns is-centered'>
-                                <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                <Link href="/use-cases">
+                                  <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                </Link>
                               </div>
                             </p>
                           </p>
@@ -124,7 +130,9 @@ const Homepage = () => {
                             </p>
                             <p className="box has-background-black-ter pt-4 pb-3">
                               <div className='columns is-centered'>
-                                <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                <Link href="/use-cases">
+                                  <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                </Link>
                               </div>
                             </p>
                           </p>
@@ -138,7 +146,9 @@ const Homepage = () => {
                             </p>
                             <p className="box has-background-black-ter pt-4 pb-3">
                               <div className='columns is-centered'>
-                                <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                <Link href="/use-cases">
+                                  <button className="button is-primary is-responsive is-outlined py-2 px-6 is-size-6">Details </button>
+                                </Link>
                               </div>
                             </p>
                           </p>
