160dfe072e23d4ac6e1250dd34f979b59818d91d
Vito <farinavito@gmail.com>
2022-03-10 15:04:06 +0100

    created page also for community and deployed-features

---
+++ pages/community.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Community.module.css'
+import 'bulma/css/bulma.css'
+import Link from 'next/link'
+
+const Community = () => {
+
+    return(
+        <div className={styles.main}>
+            <p>community</p>
+        </div>
+    )
+}
+
+export default Community
---
+++ pages/deployed-features.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Features.module.css'
+import 'bulma/css/bulma.css'
+import Link from 'next/link'
+
+const Features = () => {
+
+    return(
+        <div className={styles.main}>
+            <p>features</p>
+        </div>
+    )
+}
+
+export default Features
