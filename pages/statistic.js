787b0b384cf07eeeb1765d8ddd63b301e6506582
Vito <farinavito@gmail.com>
2022-03-11 21:08:40 +0100

    added navbar to the statistic

--- pages/statistic.js
+++ pages/statistic.js
@@ -7,7 +7,75 @@ const Statistic = () => {
 
     return(
         <div className={styles.main}>
-            <p>stats</p>
+            <nav className="navbar has-background-black-ter is-fixed-top">
+                <div className="navbar-menu">
+                    <div className="container">
+                        <div className="navbar-brand">
+                        <div className='navbar-item'></div>
+                        <div className='navbar-item'></div>
+                        <div className='navbar-item'></div>
+                        <div className='navbar-item'></div>
+                          <div className='navbar-item'>
+                            <Link href='/'>
+                                <a><h1>A web app</h1></a>
+                            </Link>
+                          </div> 
+                        </div>
+                    </div> 
+                </div>
+                
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/why'>
+                        <a>Why</a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/use-cases'>
+                        <a>Use cases</a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/how-it-works'>
+                        <a>How it works</a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/deployed-features'>
+                        <a>Features</a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/statistic'>
+                        <a>Statistic</a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/community'>
+                        <a>Community</a>
+                    </Link>
+                  </div>
+                </div>
+                <div className="navbar-end">
+                  <div className='navbar-item'>
+                    <Link href='/application'>
+                        <a>Enter app</a>
+                    </Link>
+                  </div>
+                  <div className='navbar-item'>
+                  </div>
+                </div>
+            </nav>
         </div>
     )
 }
